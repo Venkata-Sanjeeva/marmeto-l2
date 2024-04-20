206ca134bc52b8c34c2b0b5f76c71588a6fe6f82
Venkata-Sanjeeva <152136689+Venkata-Sanjeeva@users.noreply.github.com>
2024-04-20 17:21:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello");
+const cartBtn = document.querySelector('.cart-btn');
+
+cartBtn.addEventListener('click',() => {
+    const msg = document.querySelector('.pop-up-msg');
+    msg.classList.add('js-pop-up-msg');
+    msg.innerText = 'Embrace Sideboard with Color Yellow and Size Small added to cart';
+})
